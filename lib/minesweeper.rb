196617e0accdea3e02e7ab09650e7426fac712e1
Samuel Prado <darkms@gmail.com>
2017-04-27 15:19:42 -0300

    Add the game flow for Minesweeper class

--- lib/minesweeper.rb
+++ lib/minesweeper.rb
@@ -1,13 +1,43 @@
 # frozen_string_literal: true
 
 require 'board'
+
 # Minesweeper class is responsible to control the game flow
 class Minesweeper
-  def initialize(width:, height:, mines:)
-    @board = Board.new(width: width, height: height, mine_count: mines)
+  def initialize(width, height, mines)
+    @board = Board.new(width: width,
+                       height: height,
+                       mine_count: mines,
+                       minesweeper: self)
+    @state = :playing
+  end
+
+  def play(x_axis, y_axis)
+    board.click_tile(x_axis: x_axis, y_axis: y_axis)
+  end
+
+  def flag(x_axis, y_axis)
+    board.flag_tile(x_axis: x_axis, y_axis: y_axis)
+  end
+
+  def mine_clicked
+    self.state = :lose
+  end
+
+  def victory
+    self.state = :win
+  end
+
+  def victory?
+    state == :win
+  end
+
+  def still_playing?
+    state == :playing
   end
 
   private
 
   attr_reader :board
+  attr_accessor :state
 end
--- spec/lib/minesweeper_spec.rb
+++ spec/lib/minesweeper_spec.rb
@@ -3,16 +3,70 @@
 require 'spec_helper'
 
 describe Minesweeper do
-  subject { Minesweeper }
-
   let(:width) { 10 }
   let(:height) { 10 }
   let(:mines) { 20 }
 
+  subject { Minesweeper.new(width, height, mines) }
+
   describe '#initialize' do
     it 'should create a board' do
-      expect(Board).to receive(:new).with(width: width, height: height, mine_count: mines)
-      subject.new(width: width, height: height, mines: mines)
+      params = { width: width,
+                 height: height,
+                 mine_count: mines,
+                 minesweeper: anything }
+      expect(Board).to receive(:new).with(params)
+      subject
+    end
+  end
+
+  describe '#play' do
+    it 'should receive x_axis and y_axis and click to a tile on board' do
+      expect_any_instance_of(Board).to receive(:click_tile).and_return true
+      subject.play(rand(width), rand(height))
+    end
+  end
+
+  describe '#flag' do
+    it 'should receive x_axis and y_axis and flag a tile on board' do
+      expect_any_instance_of(Board).to receive(:flag_tile).and_return true
+      subject.flag(rand(width), rand(height))
+    end
+  end
+
+  describe '#mine_clicked' do
+    before :each do
+      subject.mine_clicked
+    end
+
+    context 'checking victory' do
+      it 'should return false' do
+        expect(subject.victory?).to be false
+      end
+    end
+
+    context 'checking if still playing' do
+      it 'should return false' do
+        expect(subject.still_playing?).to be false
+      end
+    end
+  end
+
+  describe '#victory' do
+    before :each do
+      subject.victory
+    end
+
+    context 'checking victory' do
+      it 'should return true' do
+        expect(subject.victory?).to be true
+      end
+    end
+
+    context 'checking if still playing' do
+      it 'should return false' do
+        expect(subject.still_playing?).to be false
+      end
     end
   end
 end
