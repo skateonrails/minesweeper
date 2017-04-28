c20cf1695273c19cd27b46b754b040c50beb09b9
Samuel Prado <darkms@gmail.com>
2017-04-28 15:35:33 -0300

    Create a dependencies file to load all dependencies

---
+++ dependencies.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# requiring printers
+require_relative 'printers/simple_printer'
+require_relative 'printers/pretty_printer'
+
+# requiring lib files
+require_relative 'lib/tile'
+require_relative 'lib/board'
+require_relative 'lib/minesweeper'
